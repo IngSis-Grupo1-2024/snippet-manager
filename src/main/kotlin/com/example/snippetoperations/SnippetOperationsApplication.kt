8a428d32499f2c63bae2faaf389db12037db436d
franciscocavallaro <ccavallaro00@gmail.com>
2024-04-29 20:49:38 -0300

    Initial commit

---
+++ src/main/kotlin/com/example/snippetoperations/SnippetOperationsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.snippetoperations
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SnippetOperationsApplication
+
+fun main(args: Array<String>) {
+    runApplication<SnippetOperationsApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/snippetoperations/SnippetOperationsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.snippetoperations
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SnippetOperationsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
