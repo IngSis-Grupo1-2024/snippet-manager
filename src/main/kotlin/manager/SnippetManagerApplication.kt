ea25aa0a46d9b9934786f3a6ca91266bd3762c57
constanzalasarte <constanza.lasarte@ing.austral.edu.ar>
2024-06-22 18:35:04 -0300

    updated the application.yaml and env-template

--- src/main/kotlin/manager/SnippetManagerApplication.kt
+++ src/main/kotlin/manager/SnippetManagerApplication.kt
@@ -6,20 +6,6 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class SnippetManagerApplication
 
-fun main(args: Array<String>) {
-    runApplication<manager.SnippetManagerApplication>()
-
-//    val azuriteBucketV1 = System.getenv("AZURITE_BUCKET_V1")
-//    val restTemplate = RestTemplate()
-//    val bucketManager = BucketManager(azuriteBucketV1, restTemplate)
-
-//    val snippet = CreateSnippet("basic-snippet", "println('Hello World')", "ps", "1.0.0")
-//    val response = bucketManager.createSnippet("1", snippet.content)
-//    val response = bucketManager.getSnippet("1")
-//    val response = bucketManager.deleteSnippet("1")
-//
-//    println(response.statusCode)
-//    println(response.headers)
-//    println(response.body)
-
+fun main() {
+    runApplication<SnippetManagerApplication>()
 }
