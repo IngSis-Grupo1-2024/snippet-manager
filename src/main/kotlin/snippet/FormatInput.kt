479d2587fd5ce1df69ec9d3672d5927ece7b8868
valentinavalenzi <92260481+valentinavalenzi@users.noreply.github.com>
2024-06-28 17:29:58 -0300

    FormatInput added

---
+++ src/main/kotlin/snippet/FormatInput.kt
@@ -0,0 +1,12 @@
+package snippet
+
+import manager.manager.model.enums.SnippetLanguage
+import manager.rules.dto.RulesDTO
+
+class FormatInput(
+    val content: String,
+    val language: SnippetLanguage,
+    val version: String,
+    val rules: List<RulesDTO>,
+    val input: List<String>,
+)
