8d7c1ce8bd6d56c2d9b614b5b900b645e4f5dbee
constanzalasarte <clasarte@salesforce.com>
2024-07-03 22:25:42 -0300

    add the prod dns origin

--- src/main/kotlin/manager/SnippetManagerApplication.kt
+++ src/main/kotlin/manager/SnippetManagerApplication.kt
@@ -20,7 +20,10 @@ class SnippetManagerApplication {
         return object : WebMvcConfigurer {
             override fun addCorsMappings(registry: CorsRegistry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:5173", "https://snippet-runner-dev.duckdns.org/")
+                    .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://snippet-runner-dev.duckdns.org/",
+                        "https://snippet-runner.duckdns.org/")
                     .allowedMethods("*")
                     .allowedHeaders("*")
             }
